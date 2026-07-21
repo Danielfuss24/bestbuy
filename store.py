a579f635a3f5e3b2cd4be022fb8136c0eab31dc3
Daniel Fuß <danielfuss24@gmail.com>
2026-07-21 12:40:22 +0200

    Add active product listing

--- store.py
+++ store.py
@@ -1,5 +1,6 @@
 from products import Product
 
+
 class Store:
     def __init__(self, product_list):
         self.product_list = product_list
@@ -9,6 +10,23 @@ class Store:
 
     def remove_product(self, product):
         self.product_list.remove(product)
+
+    def get_total_quantity(self) -> int:
+        total_quantity = 0
+
+        for product in self.product_list:
+            total_quantity += product.get_quantity()
+
+        return total_quantity
+
+    def get_all_products(self) -> list[Product]:
+        active_products = []
+
+        for product in self.product_list:
+            if product.is_active():
+                active_products.append(product)
+
+        return active_products
 
 if __name__ == "__main__":
     bose = Product(
@@ -45,4 +63,11 @@ if __name__ == "__main__":
 
     print("\nNach dem Entfernen:")
     for product in best_buy.product_list:
+        product.show()
+
+    print("\nGesamtanzahl im Store:")
+    print(best_buy.get_total_quantity())
+
+    print("\nAktive Produkte:")
+    for product in best_buy.get_all_products():
         product.show()
